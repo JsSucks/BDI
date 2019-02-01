f370f46c21f6bc6d31959332cdd52ac361b01d41
Jiiks <jiiks@windowslive.com>
2019-02-02 00:45:45 +0200

    add config

---
+++ BDI/config.h
@@ -0,0 +1,14 @@
+/* BetterDiscord Installer
+ *
+ * Copyright (c) 2019-present Jiiks - https://github.com/Jiiks
+ * All rights reserved.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+class Config final {
+public:
+	Config();
+};
