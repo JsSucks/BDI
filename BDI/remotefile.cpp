8275d610b2e97d4f8f6d2a51fb0a90592500e507
Jiiks <jiiks@windowslive.com>
2019-02-03 16:34:53 +0200

    add remote file

---
+++ BDI/remotefile.cpp
@@ -0,0 +1,109 @@
+/* BetterDiscord Installer
+ *
+ * Copyright (c) 2019-present Jiiks - https://github.com/Jiiks
+ * All rights reserved.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "remotefile.h"
+#include "logger.h"
+
+RemoteFile::RemoteFile(const QString &fileName, const QString &basePath, const QUrl &remoteLocation, const bool memoryOnly) {
+	_memoryOnly = memoryOnly;
+	_fileName = fileName;
+	_basePath = QDir(QDir::toNativeSeparators(basePath));
+	_remoteLocation = remoteLocation;
+
+	connect(&_manager, &QNetworkAccessManager::finished, this, &RemoteFile::downloadFinished);
+}
+
+void RemoteFile::download() {
+	Logger::Debug("Fetching remote file: " + _remoteLocation.toString());
+	const QNetworkRequest request(_remoteLocation);
+	const auto reply = _manager.get(request);
+
+	connect(reply, &QNetworkReply::downloadProgress, [=](const qint64 bytesReceived, const qint64 bytesTotal) {
+		if(bytesTotal < 0) return;
+		emit downloadProgressChanged(
+			bytesReceived,
+			bytesTotal,
+			bytesReceived * 100 / bytesTotal,
+			_systemLocale.formattedDataSize(bytesReceived),
+			_systemLocale.formattedDataSize(bytesTotal)
+		);
+	});
+}
+
+void RemoteFile::downloadFinished(QNetworkReply * reply) {
+	auto url = reply->url();
+
+	auto statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+
+	Logger::Debug("Finished fetch: " + url.toString() + " - " + statusCode.toString());
+
+	if(reply->error()) {
+		_error = reply->errorString();
+		emit error(this);
+		return;
+	}
+
+	if(_memoryOnly) {
+		_bytes = reply->readAll();
+		emit finished(this);
+		return;
+	}
+
+	if(_fileName.isEmpty()) {
+		_fileName = QFileInfo(url.path()).fileName();
+	}
+
+	_localFilePath = _basePath.filePath(_fileName);
+
+	QFile f(_localFilePath);
+	if(!f.open(QIODevice::WriteOnly)) {
+		_error = "Cannot write to: " + _localFilePath;
+		emit error(this);
+		return;
+	}
+
+	f.write(reply->readAll());
+	f.close();
+
+	emit finished(this);
+}
+
+QString RemoteFile::readAll() const {
+	if(_memoryOnly) return _bytes;
+
+	if(!QFile::exists(_localFilePath)) return "";
+	QFile f(_localFilePath);
+	if(!f.open(QFile::ReadOnly | QFile::Text)) return "";
+	auto ra = f.readAll();
+	f.close();
+	return ra;
+}
+
+QByteArray RemoteFile::hash(const QCryptographicHash::Algorithm & algorithm) {
+	if(!_hash.isEmpty()) return _hash;
+
+	QCryptographicHash hash(algorithm);
+	if(!_memoryOnly) {
+		QFile f(_localFilePath);
+		f.open(QFile::ReadOnly);
+		hash.addData(&f);
+		f.close();
+		return (_hash = hash.result().toHex());
+	}
+	hash.addData(_bytes);
+	return (_hash = hash.result().toHex());
+}
+
+QString RemoteFile::hashString(const QCryptographicHash::Algorithm & algorithm) {
+	return QString::fromUtf8(hash(algorithm));
+}
+
+bool RemoteFile::compareHash(const QString & hash, const bool &recalculate) {
+	if(recalculate) _hash.clear();
+	return hashString().compare(hash, Qt::CaseInsensitive) == 0;
+}
---
+++ BDI/remotefile.h
@@ -0,0 +1,62 @@
+/* BetterDiscord Installer
+ *
+ * Copyright (c) 2019-present Jiiks - https://github.com/Jiiks
+ * All rights reserved.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QUrl>
+#include <QLocale>
+#include "qtfs.hpp"
+
+class RemoteFile final : public QObject {
+	Q_OBJECT
+		QNetworkAccessManager _manager;
+
+public:
+	RemoteFile(const QString &fileName, const QString &basePath, const QUrl &remoteLocation, bool memoryOnly = true);
+	RemoteFile(const QString &basePath, const QUrl &remoteLocation, const bool memoryOnly = true) : RemoteFile("", basePath, remoteLocation, memoryOnly) {};
+	explicit RemoteFile(const QUrl &remoteLocation, const bool memoryOnly = true) : RemoteFile("", "", remoteLocation, memoryOnly) {};
+
+	void download();
+	QString readAll() const;
+	QByteArray hash(const QCryptographicHash::Algorithm &algorithm = QCryptographicHash::Sha256);
+	QString hashString(const QCryptographicHash::Algorithm &algorithm = QCryptographicHash::Sha256);
+	bool compareHash(const QString &hash, const bool &recalculate = false);
+
+	QUrl remoteLocation() const { return _remoteLocation; }
+	QString fileName() const { return _fileName; }
+	QDir basePath() const { return _basePath; }
+	QString localFilePath() const { return _localFilePath; }
+	bool localFileExists() const { return QFile(_localFilePath).exists(); }
+	QFile &localFile() const { return QFile(_localFilePath); }
+	bool isMemoryOnly() const { return _memoryOnly; }
+	QString errorMsg() const { return _error; }
+	QByteArray bytes() const { return _bytes; }
+
+private:
+	QUrl _remoteLocation;
+	QString _fileName;
+	QDir _basePath;
+	QString _localFilePath;
+	bool _memoryOnly;
+	QLocale _systemLocale = QLocale::system();
+	QByteArray _bytes;
+	QString _error;
+	QByteArray _hash;
+
+public slots:
+	void downloadFinished(QNetworkReply *reply);
+
+signals:
+	void downloadProgressChanged(qint64 bytesReceived, qint64 bytesTotal, double percentage, QString bytesReceivedFormatted, QString bytesTotalFormatted);
+	void finished(RemoteFile *remoteFile);
+	void error(RemoteFile *remoteFile);
+};
