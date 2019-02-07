241b61a3eb08b0554c775b586665e703cb239a31
Jiiks <jiiks@windowslive.com>
2019-02-07 09:17:49 +0200

    Update headers

--- src/logger.h
+++ src/logger.h
@@ -1,10 +1,12 @@
-/* BetterDiscord Installer
- *
- * Copyright (c) 2019-present Jiiks - https://github.com/Jiiks
+/**
+ * BetterDiscord Installer
+ * Copyright (c) 2018-present Jiiks/JsSucks - https://github.com/Jiiks / https://github.com/JsSucks
  * All rights reserved.
+ * https://betterdiscord.net
+ *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- */
+*/
 
 #pragma once
 
